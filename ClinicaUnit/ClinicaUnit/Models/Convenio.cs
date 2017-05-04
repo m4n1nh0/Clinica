ae6576beb5eb95e649908697e9bd3de97e82454e
Mariano Florencio <marianofmendonca@gmail.com>
2017-05-04 17:19:18 -0300

    Models 1.0

---
+++ ClinicaUnit/ClinicaUnit/Models/Convenio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicaUnit.Models
+{
+    /*id, nome, sigla, telefone*/
+    public class Convenio
+    {
+    }
+}
---
+++ ClinicaUnit/ClinicaUnit/Models/Exame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicaUnit.Models
+{
+    /* id, nome, obs */
+    public class Exame
+    {
+    }
+}
